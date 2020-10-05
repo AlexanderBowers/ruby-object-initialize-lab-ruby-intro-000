b94f454e69ec76801f13013cf3ea40e35f00a68c
Alexander Bowers <7319abowers@gmail.com>
2020-10-05 02:40:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,11 @@
 class Person
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+  end
 
-def name
-  @name = name
-end
+  def name
+    @name = name
+  end
+
 end
